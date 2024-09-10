03b0beb3cf7a4e3e57084fcd5ea3b41609e54531
SooinPark1019 <95750164+SooinPark1019@users.noreply.github.com>
2024-09-10 13:57:05 +0900

    [Diamond IV] Title: Railway Trip 2, Time: 1828 ms, Memory: 161460 KB, Score: 27 point -BaekjoonHub

--- 백준/Diamond/24477. Railway Trip 2/Railway Trip 2.cc
+++ 백준/Diamond/24477. Railway Trip 2/Railway Trip 2.cc
@@ -1,16 +1,16 @@
 #include <ext/rope>
 #include <bits/stdc++.h>
 #define _USE_MATH_DEFINES
-#pragma GCC optimize ("O3")
-#pragma GCC optimize ("Ofast")
-#pragma GCC optimize ("unroll-loops")
+//#pragma GCC optimize ("O3")
+//#pragma GCC optimize ("Ofast")
+//#pragma GCC optimize ("unroll-loops")
 #define pii pair<int,int>
 #define pll pair<ll,ll>
 #define PB push_back
 #define fi first
 #define se second
 #define all(v) (v).begin(), (v).end()
-#define int long long
+//#define int long long
 
 using namespace __gnu_cxx;
 using namespace std;
@@ -23,30 +23,28 @@ const double EPS = 1e-9;
 const ll LNF = 1e18;
 const ll mod = 998244353;
 
-vector<vector<pll>> seg(20, vector<pll>(400005, {1e18, -1e18}));
-vector<vector<pll>> lazy(20, vector<pll>(400005, {1e18, -1e18}));
+vector<vector<pii>> seg(20, vector<pii>(400005, {1e9, -1e9}));
+vector<vector<pii>> lazy(20, vector<pii>(400005, {1e9, -1e9}));
 
-pll mergepair(pll A, pll B){
-    //cout << A.first << " " << A.second << " " << B.first << " " << B.second << "\n";
-    //cout << min(A.first, B.first) << " " << max(A.second, B.second) << "\n";
+inline pii mergepair(pii A, pii B){
     return {min(A.first, B.first), max(A.second, B.second)};
 }
 
 void LazyUpdate(int node, int start, int end, int num){
     //cout << node << " " << start << " " << end << " " << num << "\n";
     //cout << lazy[num][node].first << " " << lazy[num][node].second << "\n";
-    if(lazy[num][node].first!=1e18||lazy[num][node].second!=1e18){
+    if(lazy[num][node].first!=1e9||lazy[num][node].second!=-1e9){
         seg[num][node]=mergepair(seg[num][node], lazy[num][node]);
         //cout << seg[num][node].first << " " << seg[num][node].second << "\n";
         if(start!=end){
             lazy[num][node*2]=mergepair(lazy[num][node*2], lazy[num][node]);
             lazy[num][node*2+1]=mergepair(lazy[num][node*2+1], lazy[num][node]);
         }
-        lazy[num][node]={1e18, -1e18};
+        lazy[num][node]={1e9, -1e9};
     }   
 }
 
-void updateseg(int node, int start, int end, int num, int left, int right, ll val1, ll val2){
+void updateseg(int node, int start, int end, int num, int left, int right, int val1, int val2){
     LazyUpdate(node, start, end, num);
     if(right<start||end<left) return;
     else if(left<=start&&end<=right){
@@ -64,10 +62,10 @@ void updateseg(int node, int start, int end, int num, int left, int right, ll va
     return;
 }
 
-pll sumseg(int node, int start, int end, int num, int left, int right){
+pii sumseg(int node, int start, int end, int num, int left, int right){
     //cout << node << " " << start << " " << end << " " << num << " " << seg[node][num].first << " " << seg[node][num].second << "\n";
     LazyUpdate(node, start, end, num);
-    if(right<start||end<left) return {1e18, -1e18};
+    if(right<start||end<left) return {1e9, -1e9};
     else if(left<=start&&end<=right){
         //cout << node << " " << start << " " << end << " " << num << " " << seg[num][num].first << " " << seg[node][num].second << "\n";
         return seg[num][node];
@@ -85,9 +83,9 @@ signed main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    ll N, K;
+    int N, K;
     cin >> N >> K;
-    ll M;
+    int M;
     cin >> M;
 
     for(int i=0; i<20; i++){
@@ -102,10 +100,10 @@ signed main(){
         int a, b;
         cin >> a >> b;
         if(a<b){
-            updateseg(1, 1, N, 0, a, min(a+K-1, b), 1e18, b);
+            updateseg(1, 1, N, 0, a, min(a+K-1, b), 1e9, b);
         }
         else{
-            updateseg(1, 1, N, 0, max(a-K+1, b), a, b, -1e18);
+            updateseg(1, 1, N, 0, max(a-K+1, b), a, b, -1e9);
         }
     }
 
@@ -116,10 +114,10 @@ signed main(){
 
     for(int i=1; i<20; i++){
         for(int j=1; j<=N; j++){
-            updateseg(1, 1, N, i, j, j, sparsetable[j][i-1].first, sparsetable[j][i-1].second);
+            sparsetable[j][i]=sumseg(1, 1, N, i-1, sparsetable[j][i-1].first, sparsetable[j][i-1].second);
         }
         for(int j=1; j<=N; j++){
-            sparsetable[j][i]=sumseg(1, 1, N, i, sparsetable[j][i-1].first, sparsetable[j][i-1].second);
+            updateseg(1, 1, N, i, j, j, sparsetable[j][i].first, sparsetable[j][i].second);
         }
     }
 
@@ -128,17 +126,17 @@ signed main(){
     while(Q--){
         int S, T;
         cin >> S >> T;
-        ll left=0;
-        ll right=M+5;
-        ll ans=987654321;
+        int left=0;
+        int right=M+5;
+        int ans=987654321;
         while(left<=right){
-            ll mid=(left+right)/2;
-            ll temp=mid;
+            int mid=(left+right)/2;
+            int temp=mid;
             pll a={S, S};
             int i=0;
             while(temp!=0){
                 if((temp&1)!=0){
-                    a=sumseg(1, 1, N, i+1, a.first, a.second);
+                    a=sumseg(1, 1, N, i, a.first, a.second);
                 }
                 i++;
                 temp>>=1;
