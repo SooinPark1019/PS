ce21b5c08a1f060b072ad58bac02bebdf7f301f7
SooinPark1019 <95750164+SooinPark1019@users.noreply.github.com>
2024-11-16 07:58:12 +0900

    [Platinum IV] Title: 초고속철도, Time: 44 ms, Memory: 6004 KB -BaekjoonHub

---
+++ 백준/Platinum/2543. 초고속철도/초고속철도.cc
@@ -0,0 +1,78 @@
+#include <ext/rope>
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES
+//#pragma GCC optimize ("O3")
+//#pragma GCC optimize ("Ofast")
+//#pragma GCC optimize ("unroll-loops")
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define PB push_back
+#define fi first
+#define se second
+#define all(v) (v).begin(), (v).end()
+#define int long long
+
+using namespace __gnu_cxx;
+using namespace std;
+
+typedef long long ll;
+//typedef __int128 i128;
+typedef long double ld;
+const int INF = 0x3f3f3f3f;
+const double EPS = 1e-9;
+const ll LNF = 1e18;
+const ll mod = 20070713;
+const int MAXN = (1<<18)+5;
+const int D = 20;
+
+/*
+1을 루트로 하는 트리를 생각하자
+엘리스는 부모로 올라갈 것이고
+퀸은 리프와의 최단 경로로 끌어내릴 것이다
+각 정점마다 부모와의 거리, 리프와의 최단 경로를 적어 두자
+*/
+
+int DP[100005];
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int N;
+    cin >> N;
+    vector<pii> V;
+    for(int i=0; i<N; i++){
+        int a, b;
+        cin >> a >> b;
+        V.push_back({a, b});
+    }
+
+    sort(all(V));
+
+    DP[0]=1;
+
+    for(int i=0; i<N; i++){
+        DP[i+1]+=DP[i];
+        DP[i+1]%=mod;
+        int left=i+1;
+        int right=N-1;
+        int ans=N;
+        while(left<=right){
+            int mid=(left+right)>>1;
+            if(V[mid].first>V[i].second){
+                ans=min(ans, mid);
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        DP[ans]+=DP[i];
+        DP[ans]%=mod;
+    }
+
+    cout << DP[N];
+    
+    return 0;
+}
