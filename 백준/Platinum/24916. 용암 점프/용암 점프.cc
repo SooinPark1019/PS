04ed84acc32a66536081bdc005bd4518dd80b074
SooinPark1019 <95750164+SooinPark1019@users.noreply.github.com>
2024-12-10 22:06:14 +0900

    [Platinum III] Title: 용암 점프, Time: 200 ms, Memory: 2420 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/24916. 용암 점프/용암 점프.cc
+++ 백준/Platinum/24916. 용암 점프/용암 점프.cc
@@ -11,7 +11,7 @@
 #define se second
 #define all(v) (v).begin(), (v).end()
 #define zip(v) sort(all(v)); v.erase(unique(all(v)), v.end());
-#define int long long
+//#define int long long
 #define sz(x) (int)x.size()
 
 using namespace __gnu_cxx;
@@ -49,39 +49,36 @@ signed main(){
             for(int i=0; i<N; i++) cout << "NO\n";
         }
         else{
-            for(int a=1; a<=N; a++){
-                for(int i=0; i<=N+1; i++){
-                    for(int j=0; j<=N+1; j++){
-                        DP[i][j][0]=1e18;
-                        DP[i][j][1]=1e18;
-                    }
-                }
-                DP[a-1][a+1][0]=0;
-                for(int i=0; i<=N; i++){
-                    for(int j=0; j+i<=N+1; j++){
-                        for(int k=0; k<2; k++){
-                            if(DP[j][i+j][k]==1e18) continue;
-                            int cur;
-                            if(k==0) cur=j+1;
-                            else cur=j+i-1;
-                            if(j>0&&abs(V[cur]-V[j])>=DP[j][i+j][k]*2){
-                                DP[j-1][i+j][0]=min(DP[j-1][i+j][0], abs(V[cur]-V[j]));
+            memset(DP, -1, sizeof(DP));
+            DP[1][N][0]=1e9;
+            DP[1][N][1]=1e9;
+            for(int i=N-1; i>0; i--){
+                for(int j=1; j+i<=N; j++){
+                    for(int k=0; k<2; k++){
+                        if(DP[j][i+j][k]==-1) continue;
+                        //cout << j << " " << i+j << " " << k << " " << DP[j][j+i][k] << "\n";
+                        if(k==0){
+                            if(abs(V[j]-V[j+1])*2<=DP[j][i+j][k]){
+                                DP[j+1][i+j][0]=max(DP[j+1][i+j][0], abs(V[j]-V[j+1]));
                             }
-                            if(i+j<=N&&abs(V[cur]-V[i+j])>=DP[j][i+j][k]*2){
-                                DP[j][i+j+1][1]=min(DP[j][i+j+1][1], abs(V[cur]-V[i+j]));
+                            if(abs(V[j]-V[i+j])*2<=DP[j][i+j][k]){
+                                DP[j+1][i+j][1]=max(DP[j+1][i+j][1], abs(V[j]-V[i+j]));
+                            }
+                        }
+                        else{
+                            if(abs(V[j]-V[i+j])*2<=DP[j][i+j][k]){
+                                DP[j][i+j-1][0]=max(DP[j][i+j-1][0], abs(V[j]-V[i+j]));
+                            }
+                            if(abs(V[i+j-1]-V[i+j])*2<=DP[j][i+j][k]){
+                                DP[j][i+j-1][1]=max(DP[j][i+j-1][1], abs(V[i+j-1]-V[i+j]));
                             }
                         }
                     }
                 }
-                int flag=0;
-                for(int i=0; i<2; i++){
-                    if(DP[0][N+1][i]!=1e18){
-                        flag=1;
-                        break;
-                    }
-                }
-                if(flag==0) cout << "NO\n";
-                else cout << "YES\n";
+            }
+            for(int i=1; i<=N; i++){
+                if(DP[i][i][0]!=-1||DP[i][i][1]!=-1) cout << "YES\n";
+                else cout << "NO\n";
             }
         }
     }
