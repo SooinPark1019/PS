e5006b0823cc9ead09a20c7064b16276df0d6393
SooinPark1019 <95750164+SooinPark1019@users.noreply.github.com>
2024-12-10 20:28:48 +0900

    [Platinum III] Title: 용암 점프, Time: 192 ms, Memory: 2820 KB, Score: 52 point -BaekjoonHub

--- 백준/Platinum/24916. 용암 점프/용암 점프.cc
+++ 백준/Platinum/24916. 용암 점프/용암 점프.cc
@@ -30,7 +30,7 @@ const double PI = acos(-1);
 
 /*
 */
-int DP[30][30][30]={};
+int DP[30][30][2]={};
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -52,30 +52,30 @@ signed main(){
             for(int a=1; a<=N; a++){
                 for(int i=0; i<=N+1; i++){
                     for(int j=0; j<=N+1; j++){
-                        for(int k=0; k<=N+1; k++){
-                            DP[i][j][k]=1e18;
-                        }
+                        DP[i][j][0]=1e18;
+                        DP[i][j][1]=1e18;
                     }
                 }
-                DP[a-1][a][a+1]=0;
+                DP[a-1][a+1][0]=0;
                 for(int i=0; i<=N; i++){
                     for(int j=0; j+i<=N+1; j++){
-                        for(int k=j+1; k<=j+i-1; k++){
-                            if(DP[j][k][j+i]==1e18) continue;
-                            //cout << "j : " << j << " " << " k : " << k << " j+i : " << j+i << " DP : " << DP[j][k][j+i]  << "\n";
-                            //cout << abs(V[j]-V[k]) << "\n";
-                            if(j>0&&abs(V[j]-V[k])>=DP[j][k][j+i]*2){
-                                DP[j-1][j][j+i]=min(DP[j-1][j][j+i], abs(V[j]-V[k]));
+                        for(int k=0; k<2; k++){
+                            if(DP[j][i+j][k]==1e18) continue;
+                            int cur;
+                            if(k==0) cur=j+1;
+                            else cur=j+i-1;
+                            if(j>0&&abs(V[cur]-V[j])>=DP[j][i+j][k]*2){
+                                DP[j-1][i+j][0]=min(DP[j-1][i+j][0], abs(V[cur]-V[j]));
                             }
-                            if(j+i<=N&&abs(V[i+j]-V[k])>=DP[j][k][j+i]*2){
-                                DP[j][j+i][j+i+1]=min(DP[j][j+i][j+i+1], abs(V[i+j]-V[k]));
+                            if(i+j<=N&&abs(V[cur]-V[i+j])>=DP[j][i+j][k]*2){
+                                DP[j][i+j+1][1]=min(DP[j][i+j+1][1], abs(V[cur]-V[i+j]));
                             }
                         }
                     }
                 }
                 int flag=0;
-                for(int i=1; i<=N; i++){
-                    if(DP[0][i][N+1]<1e18){
+                for(int i=0; i<2; i++){
+                    if(DP[0][N+1][i]!=1e18){
                         flag=1;
                         break;
                     }
