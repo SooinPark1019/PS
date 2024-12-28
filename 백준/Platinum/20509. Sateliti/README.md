6e2693c300d206ed92395d8f9cd7d382b08287fe
SooinPark1019 <95750164+SooinPark1019@users.noreply.github.com>
2024-12-28 18:16:31 +0900

    [Platinum I] Title: Sateliti, Time: 292 ms, Memory: 17808 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Platinum/20509. Sateliti/README.md
@@ -0,0 +1,34 @@
+# [Platinum I] Sateliti - 20509 
+
+[문제 링크](https://www.acmicpc.net/problem/20509) 
+
+### 성능 요약
+
+메모리: 17808 KB, 시간: 292 ms
+
+### 분류
+
+해싱, 문자열, 접미사 배열과 LCP 배열
+
+### 제출 일자
+
+2024년 12월 28일 18:16:20
+
+### 문제 설명
+
+<p>For crater exploration purposes, the Arecibo telescope records images of Saturn’s satellites. The scientific team must distinguish between satellite images and group the images by satellite, but it’s not that simple because satellites could be shot from different angles.</p>
+
+<p>Captured images can be displayed as n × m matrices, filled with '<code>*</code>' (crater) and '<code>.</code>' (plain surface). We say that two images correspond to the same satellite if one can get the other by <strong>circular shifts of rows and columns</strong>.</p>
+
+<p>To make the verification process easier, scientists want to find the <strong>lexicographically smallest</strong> image corresponding to the satellite from the given image. When comparing images, we compare strings obtained by concatenating all rows of the image, where characters are compared by ASCII value.</p>
+
+### 입력 
+
+ <p>The first line contains integers n and m (1 ≤ n, m ≤ 1000), the dimensions of the image.</p>
+
+<p>Each of the following n lines contains m characters '<code>*</code>' and '<code>.</code>'. This represents the captured image.</p>
+
+### 출력 
+
+ <p>Output n lines with m characters each, the wanted lexicographically smallest image.</p>
+
---
+++ 백준/Platinum/20509. Sateliti/Sateliti.cc
@@ -0,0 +1,165 @@
+#include <ext/rope>
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES
+//#pragma GCC optimize ("O3")
+//#pragma GCC optimize ("Ofast")
+//#pragma GCC optimize ("unroll-loops")
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define PB push_back
+#define fi first
+#define se second
+#define all(v) (v).begin(), (v).end()
+#define zip(v) sort(all(v)); v.erase(unique(all(v)), v.end());
+#define int long long
+#define sz(x) (int)x.size()
+
+using namespace __gnu_cxx;
+using namespace std;
+
+typedef long long ll;
+typedef complex<double> cpx;
+//typedef __int128 i128;
+typedef long double ld;
+const int INF = 0x3f3f3f3f;
+const double EPS = 1e-9;
+const ll LNF = 1e18;
+const ll mod = 1e9+123;
+const int MAXN = (1<<18)+5;
+const double PI = acos(-1);
+
+/*
+각 단어마다 짝을 지어준 다음에
+DP[i][j]=인덱스 i, 인덱스 j인 경우의 최소 비용
+*/
+
+int p=7, q=11;
+
+int fastmul(int a, int b){
+    if(b==0) return 1;
+    if(b==1) return a;
+    if(b%2==0){
+        int temp=fastmul(a, b/2);
+        return temp*temp%mod;
+    }
+    return a*fastmul(a, b-1)%mod;
+}
+
+int prefix[1005][1005];
+int arr[1005][1005];
+
+void input(int N, int M){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            char c;
+            cin >> c;
+            if(c=='.') arr[i][j]=1;
+        }
+    }
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            arr[i+N][j]=arr[i][j];
+            arr[i][j+M]=arr[i][j];
+            arr[i+N][j+M]=arr[i][j];
+        }
+    }
+}
+
+void precalc(int N, int M){
+    for(int i=0; i<2*N; i++){
+        for(int j=0; j<2*M; j++){
+            if(i!=0) prefix[i][j]+=prefix[i-1][j];
+            if(j!=0) prefix[i][j]+=prefix[i][j-1];
+            if(i!=0&&j!=0) prefix[i][j]-=prefix[i-1][j-1];
+            prefix[i][j]+=mod;
+            prefix[i][j]%=mod;
+            if(arr[i][j]==1){
+                prefix[i][j]+=fastmul(p, i)*fastmul(q, j);
+                prefix[i][j]%=mod;
+            }
+        }
+    }
+}
+
+int calc(int x, int y, int dx, int dy){
+    int a=prefix[x+dx][y+dy];
+    if(x>0) a-=prefix[x-1][y+dy];
+    if(y>0) a-=prefix[x+dx][y-1];
+    if(x>0&&y>0) a+=prefix[x-1][y-1];
+    a%=mod;
+    a+=mod;
+    a%=mod;
+    return a;
+}
+
+bool isequal(int x1, int y1, int x2, int y2, int dx, int dy){
+    int a=calc(x1, y1, dx, dy)*fastmul(p, x2)%mod*fastmul(q, y2)%mod;
+    int b=calc(x2, y2, dx, dy)*fastmul(p, x1)%mod*fastmul(q, y1)%mod;
+    //cout << "a : " << a << " b : " << b << "\n";
+    return a==b;
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+
+    input(N, M);
+    precalc(N, M);
+
+    int ansx=0;
+    int ansy=0;
+    
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            //cout << "i : " << i << " j : " << j << "\n";
+            int left=0;
+            int right=N-1;
+            int ansdx=0;
+            while(left<=right){
+                int mid=(left+right)>>1;
+                if(isequal(ansx, ansy, i, j, mid, M)){
+                    ansdx=max(ansdx, mid+1);
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
+                }
+            }
+            //cout << "ansdx : " << ansdx << "\n";
+            if(ansdx==N){
+                continue;
+            }
+            left=0;
+            right=M-1;
+            int ansdy=0;
+            while(left<=right){
+                int mid=(left+right)>>1;
+                if(isequal(ansx+ansdx, ansy, i+ansdx, j, 0, mid)){
+                    ansdy=max(ansdy, mid+1);
+                    left=mid+1;
+                }
+                else right=mid-1;
+            }
+            //cout << "ansdy : " << ansdy << "\n";
+            if(arr[ansx+ansdx][ansy+ansdy]==1){
+                ansx=i;
+                ansy=j;
+            }
+            //cout << "ansx : " << ansx << " ansy : " << ansy << "\n";
+        }
+    }
+
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            if(arr[ansx+i][ansy+j]==0) cout << '*';
+            else cout << '.';
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
