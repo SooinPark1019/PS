cd70f7789ee8479f62b6f67da3a54fd23aada412
SooinPark1019 <95750164+SooinPark1019@users.noreply.github.com>
2025-02-14 07:50:04 +0900

    [Diamond III] Title: Gift Exchange, Time: 2372 ms, Memory: 208260 KB, Score: 100 point -BaekjoonHub

--- 백준/Diamond/31626. Gift Exchange/Gift Exchange.cc
+++ 백준/Diamond/31626. Gift Exchange/Gift Exchange.cc
@@ -38,6 +38,111 @@ DP[i]=첫번째 재료의 가치가 i일 때 두번째 재료의 가능한 최�
 나머지는 취할 차원만 취하거나 아니면 둘다 취해야 함
 */
 
+vector<int> minseg;
+vector<int> minlazy;
+vector<int> maxseg;
+vector<int> maxlazy;
+vector<int> sumseg;
+
+int S[500005], T[500005];
+
+void minlazy_update(int node, int start, int end){
+    if(minlazy[node]!=1e18){
+        minseg[node]=min(minseg[node], minlazy[node]);
+        if(start!=end){
+            minlazy[node*2]=min(minlazy[node*2], minlazy[node]);
+            minlazy[node*2+1]=min(minlazy[node*2+1], minlazy[node]);
+        }
+        minlazy[node]=1e18;
+    }
+}
+
+void maxlazy_update(int node, int start, int end){
+    if(maxlazy[node]!=-1e18){
+        maxseg[node]=max(maxseg[node], maxlazy[node]);
+        if(start!=end){
+            maxlazy[node*2]=max(maxlazy[node*2], maxlazy[node]);
+            maxlazy[node*2+1]=max(maxlazy[node*2+1], maxlazy[node]);
+        }
+        maxlazy[node]=-1e18;
+    }
+}
+
+void minseg_update(int node, int start, int end, int left, int right, int value){
+    minlazy_update(node, start, end);
+    if(right<start||end<left) return;
+    else if(left<=start&&end<=right){
+        minlazy[node]=min(minlazy[node], value);
+        minlazy_update(node, start, end);
+    }
+    else{
+        int mid=(start+end)>>1;
+        minseg_update(node*2, start, mid, left, right, value);
+        minseg_update(node*2+1, mid+1, end, left, right, value);
+        minseg[node]=min(minseg[node*2], minseg[node*2+1]);
+    }
+}
+
+int range_min(int node, int start, int end, int left, int right){
+    minlazy_update(node, start, end);
+    if(right<start||end<left) return 1e18;
+    if(left<=start&&end<=right) return minseg[node];
+    int mid=(start+end)>>1;
+    return min(range_min(node*2, start, mid, left, right), range_min(node*2+1, mid+1, end, left, right));
+}
+
+void maxseg_update(int node, int start, int end, int left, int right, int value){
+    maxlazy_update(node, start, end);
+    if(right<start||end<left) return;
+    else if(left<=start&&end<=right){
+        maxlazy[node]=max(maxlazy[node], value);
+        maxlazy_update(node, start, end);
+    }
+    else{
+        int mid=(start+end)>>1;
+        maxseg_update(node*2, start, mid, left, right, value);
+        maxseg_update(node*2+1, mid+1, end, left, right, value);
+        maxseg[node]=max(maxseg[node*2], maxseg[node*2+1]);
+    }
+}
+
+int range_max(int node, int start, int end, int left, int right){
+    maxlazy_update(node, start, end);
+    if(right<start||end<left) return -1e18;
+    if(left<=start&&end<=right) return maxseg[node];
+    int mid=(start+end)>>1;
+    return max(range_max(node*2, start, mid, left, right), range_max(node*2+1, mid+1, end, left, right));
+}
+
+void sumseg_update(int node, int start, int end, int index, int value){
+    if(index<start||end<index) return;
+    if(start==end) sumseg[node]+=value;
+    else{
+        int mid=(start+end)>>1;
+        sumseg_update(node*2, start, mid, index, value);
+        sumseg_update(node*2+1, mid+1, end, index, value);
+        sumseg[node]=sumseg[node*2]+sumseg[node*2+1];
+    }
+}
+
+int range_sum(int node, int start, int end, int left, int right){
+    if(right<start||end<left) return 0;
+    if(left<=start&&end<=right) return sumseg[node];
+    int mid=(start+end)>>1;
+    return range_sum(node*2, start, mid, left, right)+range_sum(node*2+1, mid+1, end, left, right);
+}
+
+struct query{
+    int l, r, i;
+};
+
+bool compare(query A, query B){
+    return A.l<B.l;
+}
+
+vector<int> Sval[1000005];
+int ans[200005];
+
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -45,44 +150,83 @@ signed main(){
 
     int N;
     cin >> N;
-    vector<int> A;
-    vector<int> B;
-    for(int i=0; i<N; i++){
-        int a;
-        cin >> a;
-        A.push_back(a);
+
+    vector<int> A(N+1);
+    vector<int> B(N+1);
+
+    for(int i=1; i<=N; i++){
+        cin >> A[i];
     }
-    for(int i=0; i<N; i++){
-        int a;
-        cin >> a;
-        B.push_back(a);
+
+    for(int i=1; i<=N; i++){
+        cin >> B[i];
     }
+
+    minseg.resize(8*N+5, 1e18);
+    minlazy.resize(8*N+5, 1e18);
+    maxseg.resize(8*N+5, -1e18);
+    maxlazy.resize(8*N+5, -1e18);
+    sumseg.resize(4*N+5, 0);
+
+    for(int i=1; i<=N; i++){
+        int a=range_max(1, 0, 2*N, B[i], A[i]);
+        if(a==-1e18) S[i]=0;
+        else S[i]=a;
+        Sval[S[i]].push_back(i);
+        maxseg_update(1, 0, 2*N, B[i], A[i], i);
+    }
+
+    for(int i=N; i>=1; i--){
+        int a=range_min(1, 0, 2*N, B[i], A[i]);
+        if(a==1e18) T[i]=N+1;
+        else T[i]=a;
+        minseg_update(1, 0, 2*N, B[i], A[i], i);
+    }
+
+    //for(int i=1; i<=N; i++) cout << S[i] << " ";
+    //cout << "\n";
+    //for(int i=1; i<=N; i++) cout << T[i] << " ";
+    //cout << "\n";
 
     int Q;
     cin >> Q;
-    while(Q--){
+    vector<query> V;
+    for(int i=0; i<Q; i++){
         int l, r;
         cin >> l >> r;
-        l--;
-        r--;
-        vector<pii> temp;
-        for(int i=l; i<=r; i++){
-            temp.push_back({A[i], B[i]});
-        }
-        sort(all(temp));
-        //for(auto p : temp) cout << p.first << " " << p.second << "\n";
-        //cout << endl;
-        int maxpos=0;
-        for(int i=0; i<temp.size()-1; i++){
-            if(maxpos<i) break;
-            for(int j=temp.size()-1; j>i; j--){
-                if(temp[i].first>=temp[j].second){
-                    maxpos=j+1;
-                    break;
-                }
+        V.push_back({l, r, i});
+    }
+    sort(all(V), compare);
+    
+    int curl=0;
+    for(auto q : V){
+        int l=q.l;
+        int r=q.r;
+        int i=q.i;
+
+        //cout << "l : " << l << " r : " << r << " i : " << i << "\n";
+
+        while(curl<l){
+            if(curl>0){
+                //cout << "curl : " << curl << " T[curl] : " << T[curl] << "\n";
+                sumseg_update(1, 0, N, curl, -1);
+                sumseg_update(1, 0, N, T[curl], 1);
             }
+            for(auto i : Sval[curl]){
+                //cout << "i : " << i << " T[i] : " << T[i] << "\n";
+                sumseg_update(1, 0, N, i, 1);
+                sumseg_update(1, 0, N, T[i], -1);
+            }
+            curl++;
         }
-        if(maxpos==temp.size()) cout << "Yes\n";
+        int a=range_sum(1, 0, N, 0, r);
+        //cout << a << "\n";
+        if(a>0) ans[i]=0;
+        else ans[i]=1;
+    }
+
+    for(int i=0; i<Q; i++){
+        if(ans[i]) cout << "Yes\n";
         else cout << "No\n";
     }
 
