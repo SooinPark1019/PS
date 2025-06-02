495b3ae2eb39220a04b286d5e5716849317df8e2
SooinPark1019 <95750164+SooinPark1019@users.noreply.github.com>
2025-06-02 15:45:38 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,66 @@
+#include <ext/rope>
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES
+//#pragma GCC optimize ("O3")
+//#pragma GCC optimize ("Ofast")
+//#pragma GCC optimize ("unroll-loops")
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define PB push_back
+#define fi first
+#define se second
+#define all(v) (v).begin(), (v).end()
+#define zip(v) sort(all(v)); v.erase(unique(all(v)), v.end());
+//#define sz(x) (int)x.size()
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define int long long
+
+using namespace std;
+
+//using namespace __gnu_pbds;
+//using namespace __gnu_cxx;
+/*#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>//이걸 코드의 윗부분에 추가
+void m_erase(ordered_set &OS, int val){
+    int index = OS.order_of_key(val);
+    ordered_set::iterator it = OS.find_by_order(index);
+    if(*it == val) OS.erase(it);
+}*/
+//다른 건 통상적인 PBDS처럼 사용하면 되지만 erase는 반드시!!! 이 함수를 통해서만 해야 함
+//m_erase(OS, 지울 원소)처럼 사용하면 된다. 꼭 주의하자.
+//얘네 쓰려면 define int long long 꺼라.
+
+typedef long long ll;
+typedef complex<double> cpx;
+//typedef __int128 i128;
+typedef long double ld;
+typedef pair<double, int> pdl;
+const int INF = 0x3f3f3f3f;
+const double EPS = 1e-9;
+const ll LNF = 1e18;
+const ll mod = 998244353;
+const int MAXN = (1<<18)+5;
+const double PI = acos(-1);
+const int FLAG_DEBUG = 0;
+
+/*
+먼저 해당 위치에서 가로 또는 세로가 시작하는지를 나타내는 배열을 하나 만든다
+가로가 시작되면 1, 세로가 시작되면 2, 아무것도 아니면 0
+
+각 묶음에 대해서 번호를 1씩 증가시키면서 정점에 할당
+*/
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    for(int i=0; i<N; i++){
+        cout << 'a';
+    }
+
+    return 0;
+}
