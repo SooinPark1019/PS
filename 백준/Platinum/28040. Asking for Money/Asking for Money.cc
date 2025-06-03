74081034698636985e35e159175f817620698068
SooinPark1019 <95750164+SooinPark1019@users.noreply.github.com>
2025-06-03 17:52:05 +0900

    [Platinum V] Title: Asking for Money, Time: 52 ms, Memory: 10096 KB -BaekjoonHub

---
+++ 백준/Platinum/28040. Asking for Money/Asking for Money.cc
@@ -0,0 +1,102 @@
+#include <ext/rope>
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES
+//#pragma GCC optimize ("O3")
+//#pragma GCC optimize ("Ofast")
+//#pragma GCC optimize ("unroll-loops")
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define PB push_back
+#define fi first
+#define se second
+#define all(v) (v).begin(), (v).end()
+#define zip(v) sort(all(v)); v.erase(unique(all(v)), v.end());
+//#define sz(x) (int)x.size()
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define int long long
+
+using namespace std;
+
+//using namespace __gnu_pbds;
+//using namespace __gnu_cxx;
+/*#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>//이걸 코드의 윗부분에 추가
+void m_erase(ordered_set &OS, int val){
+    int index = OS.order_of_key(val);
+    ordered_set::iterator it = OS.find_by_order(index);
+    if(*it == val) OS.erase(it);
+}*/
+//다른 건 통상적인 PBDS처럼 사용하면 되지만 erase는 반드시!!! 이 함수를 통해서만 해야 함
+//m_erase(OS, 지울 원소)처럼 사용하면 된다. 꼭 주의하자.
+//얘네 쓰려면 define int long long 꺼라.
+
+typedef long long ll;
+typedef complex<double> cpx;
+//typedef __int128 i128;
+typedef long double ld;
+typedef pair<double, int> pdl;
+const int INF = 0x3f3f3f3f;
+const double EPS = 1e-9;
+const ll LNF = 1e18;
+const ll mod = 1e9+7;
+const int MAXN = (1<<18)+5;
+const double PI = acos(-1);
+
+/*
+*/
+
+vector<int> graph[1005];
+vector<int> V[1005];
+int possible[1005][1005];
+
+void fill_vector(vector<int>& vis, int start, int block){
+    queue<int> Q;
+    Q.push(start);
+    vis[start]=1;
+    while(!Q.empty()){
+        int x=Q.front();
+        Q.pop();
+        for(auto i : graph[x]){
+            if(vis[i]||i==block) continue;
+            vis[i]=1;
+            Q.push(i);
+        }
+    }
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    for(int i=1; i<=N; i++){
+        int a, b;
+        cin >> a >> b;
+        graph[a].push_back(i);
+        graph[b].push_back(i);
+        V[i].push_back(a);
+        V[i].push_back(b);
+    }
+
+    for(int i=1; i<=N; i++){
+        vector<int> visA(N+1), visB(N+1), vis(N+1);
+        //cout << "i : " << i << " " << V[i][0] << " " << V[i][1] << "\n";
+        fill_vector(visA, V[i][0], i);
+        fill_vector(visB, V[i][1], i);
+        fill_vector(vis, i, 0);
+        int flag=0;
+        for(int j=1; j<=N; j++){
+            if(visA[j]&&visB[j]&&vis[j]){
+                flag=1;
+                break;
+            }
+        }
+        if(flag) cout << 'Y';
+        else cout << 'N';
+    }
+
+    return 0;
+}
