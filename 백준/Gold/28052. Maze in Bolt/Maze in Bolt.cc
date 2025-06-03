aac0773a9948dd94a9e9a6a7e39742a68c021adb
SooinPark1019 <95750164+SooinPark1019@users.noreply.github.com>
2025-06-03 17:53:04 +0900

    [Gold II] Title: Maze in Bolt, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Gold/28052. Maze in Bolt/Maze in Bolt.cc
@@ -0,0 +1,106 @@
+#include <ext/rope>
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES
+//#pragma GCC optimize ("O3")
+//#pragma GCC optimize ("Ofast")
+//#pragma GCC optimize ("unroll-loops")
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define PB push_back
+#define fi first
+#define se second
+#define all(v) (v).begin(), (v).end()
+#define zip(v) sort(all(v)); v.erase(unique(all(v)), v.end());
+//#define sz(x) (int)x.size()
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define int long long
+
+using namespace std;
+
+//using namespace __gnu_pbds;
+//using namespace __gnu_cxx;
+/*#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>//이걸 코드의 윗부분에 추가
+void m_erase(ordered_set &OS, int val){
+    int index = OS.order_of_key(val);
+    ordered_set::iterator it = OS.find_by_order(index);
+    if(*it == val) OS.erase(it);
+}*/
+//다른 건 통상적인 PBDS처럼 사용하면 되지만 erase는 반드시!!! 이 함수를 통해서만 해야 함
+//m_erase(OS, 지울 원소)처럼 사용하면 된다. 꼭 주의하자.
+//얘네 쓰려면 define int long long 꺼라.
+
+typedef long long ll;
+typedef complex<double> cpx;
+//typedef __int128 i128;
+typedef long double ld;
+typedef pair<double, int> pdl;
+const int INF = 0x3f3f3f3f;
+const double EPS = 1e-9;
+const ll LNF = 1e18;
+const ll mod = 1e9+7;
+const int MAXN = (1<<18)+5;
+const double PI = acos(-1);
+
+/*
+*/
+
+vector<string> V;
+
+bool sol(string& S){
+    for(int i=0; i<V.size(); i++){
+        //cout << "i : " << i << "\n";
+        string temp=V[i];
+        int flag=0;
+        for(int j=0; j<S.length(); j++){
+            //cout << "j : " << j << "\n";
+            int flag2=0;
+            for(int k=0; k<S.length(); k++){
+                //cout << "k : " << k << "\n";
+                //cout << S[(j+k)%S.length()] << " " << temp[k] << "\n";
+                if(S[(j+k)%S.length()]=='1'&&temp[k]=='1'){
+                    flag2=1;
+                    break;
+                }
+                if(i>0&&S[(j+k)%S.length()]=='1'&&V[i-1][k]=='1'){
+                    flag2=1;
+                    break;
+                }
+            }
+            if(flag2==0){
+                flag=1;
+                break;
+            }
+        }
+        if(!flag) return false;
+    }
+    return true;
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int R, C;
+    cin >> R >> C;
+    string S;
+    cin >> S;
+
+    for(int i=0; i<R; i++){
+        string s;
+        cin >> s;
+        V.push_back(s);
+    }
+
+    if(sol(S)){
+        cout << 'Y';
+    }
+    else{
+        reverse(all(S));
+        if(sol(S)) cout << 'Y';
+        else cout << 'N';
+    }
+
+    return 0;
+}
